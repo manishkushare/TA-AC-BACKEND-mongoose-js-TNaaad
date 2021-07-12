bd42be944e8618c6ae5e1db2debd06357349b037
manishkushare <manishkushare@outlook.com>
2021-07-12 17:32:14 +0530

    update

---
+++ block-BNaacm/sample/models/user.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    name : {
+        type: String,
+        required : true
+    },
+    email: {
+        type : String,
+        match: /@/,
+        lowerCase: true
+    },
+    age: {
+        type : Number,
+        default : 18,
+        min: 18,
+        max:30
+    }
+})
+
+const User = mongoose.model('User',userSchema);
+
+module.exports = User;
